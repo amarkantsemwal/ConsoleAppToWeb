df073d6b0ddbe9fec009626171269729c02d65bd
amarkantsem <amarkant.semwal@newvision-software.com>
2026-05-28 16:11:22 +0530

    initial commits

---
+++ ConsoleAppToWeb/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppToWeb;
+using System.Reflection.PortableExecutable;
+
+class Program
+{
+
+    static void Main(string[] arg)
+    {
+
+        CreateHostBuilder(arg).Build().Run();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webbuilder =>
+        {
+            webbuilder.UseStartup<Startup>();
+        });
+}
+
+
---
+++ ConsoleAppToWeb/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleAppToWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection serviceDescriptors) { 
+        }
+
+        public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment webHostBuilder)
+        {
+            applicationBuilder.UseRouting();
+            applicationBuilder.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => {
+                    await context.Response.WriteAsync("Welcome!");
+                });    
+            });
+        }
+
+
+        
+
+        
+    }
+}
